338cde15670d627a87ca50cee5ab82ce0825bd74
ashuforweb <ashuforweb@gmail.com>
2016-09-05 00:05:43 +0800

    Unpacking a Sequence into Separate Variables

---
+++ Chapter1.py
@@ -0,0 +1,11 @@
+'''Unpacking a Sequence into Separate Variables
+Problem
+    You have an N-element tuple or sequence that you would like to unpack into a collection
+of N variables.
+Solution
+    You can unpack any iterable or sequence in variables using simple assignment operator.
+'''
+
+p = (4,5,6,7)
+a,b,c,d = p
+print(a,b,c,d)
